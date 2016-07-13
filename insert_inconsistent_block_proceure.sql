f4010830f22cc2495c182af70cc80f57411411b0
shubham <s.gupta@cern.ch>
2016-07-13 15:31:05 +0200

    insert procedures added

---
+++ insert_inconsistent_block_proceure.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE PROCEDURE insert_inconsistent_block(dbs_block in NUMBER, Phedx_block in NUMBER, file_status in NUMBER) IS
+BEGIN
+
+  DECLARE
+	unix_time NUMBER;
+
+    BEGIN
+    
+    unix_time := ( SYSDATE - date '1970-01-01' ) * 60 * 60 * 24;
+        INSERT INTO INCONSISTENT_BLOCKS (
+				DBS_BLOCK_ID,
+				PHEDX_BLOCK_ID,
+				CONTAINS_FILES,
+				DISCOVERY_DATE)
+				VALUES (
+				dbs_block,Phedx_block,file_status,unix_time
+				);
+
+	dbms_output.put_line('BLOCK Insert Successful');		
+  
+    END;
+
+END;
