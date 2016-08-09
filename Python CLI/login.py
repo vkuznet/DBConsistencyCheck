6674c9ad6025feba587f60f0669c1d63ebf0ccd3
shubham <s.gupta@cern.ch>
2016-08-09 23:49:26 +0200

    added python CLI files

---
+++ Python CLI/login.py
@@ -0,0 +1,10 @@
+def return_credentials():
+	credentials = {
+		'username' : 'cms_dbs_tranf_ver',
+		'password' : '',
+		'dbname' : 'int2r' 
+	}
+	return credentials
+
+if __name__ == "__main__":
+	return_credentials()
