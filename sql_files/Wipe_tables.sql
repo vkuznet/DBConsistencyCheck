7dea92d29007923c718a9fd70b953e15921ade94
shubham <s.gupta@cern.ch>
2016-08-17 11:03:31 +0200

    corrected table name

--- sql_files/Wipe_tables.sql
+++ sql_files/Wipe_tables.sql
@@ -3,7 +3,7 @@ BEGIN
 
 	execute immediate 'truncate table inconsistent_blocks';
 	execute immediate 'truncate table inconsistent_files';
-	execute immediate 'truncate table invalid_files';
+	execute immediate 'truncate table invalid_dbs_blocks';
 		commit;
 
 END;
